90ffadd65778f0a4f986b19f10e3553357bb7c3f
geovty <100711167+geovty@users.noreply.github.com>
2022-03-03 04:05:23 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+   document.querySelector('.menu-btn').addEventListener('click', () => { document.querySelector('.nav-menu').classList.toggle('show') });
+   document.querySelector('.menu-btn').addEventListener('click', () => { document.querySelector('.oscurecer').classList.toggle('show') });
+   ScrollReveal().reveal('.showcase');
+   ScrollReveal().reveal('.new-cards', { delay: 500 });
+   ScrollReveal().reveal('.card-banner-one', { delay: 500 });
+   ScrollReveal().reveal('.card-banner-two', { delay: 500 });
